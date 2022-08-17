0f7c628b1a9a46b0c5342e80f130107db7f896a7
maksim789546 <maksim_789456@mail.ru>
2022-08-17 20:38:23 +0400

    Fix 'in village' status detection method
    Old method detects village only by villagers and bell block
    Fixed method detects village by villagers and POI (Point of Interesting, like a bell, beds and etc.)

--- src/main/java/org/orecruncher/dsurround/processing/scanner/VillageScanner.java
+++ src/main/java/org/orecruncher/dsurround/processing/scanner/VillageScanner.java
@@ -2,6 +2,7 @@ package org.orecruncher.dsurround.processing.scanner;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.block.entity.BedBlockEntity;
 import net.minecraft.block.entity.BellBlockEntity;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -13,13 +14,13 @@ import org.orecruncher.dsurround.lib.world.WorldUtils;
 @Environment(EnvType.CLIENT)
 public class VillageScanner {
 
-    private static final double VILLAGE_RANGE = 64;
-    private static final int SCAN_INTERVAL = 20;
+    private static final double VILLAGE_RANGE = 32;
+    private static final int SCAN_INTERVAL = 100;
 
     private boolean isInVillage;
 
     public void tick(long tickCount) {
-        // Only check once a second
+        // Only check once in five seconds
         if (tickCount % SCAN_INTERVAL != 0)
             return;
 
@@ -38,9 +39,12 @@ public class VillageScanner {
             var villagerEntities = world.getNonSpectatingEntities(VillagerEntity.class, box);
 
             if (villagerEntities.size() > 0) {
-                // We have villagers.  Now find a bell!
-                var bell = WorldUtils.getLoadedBlockEntities(world, blockEntity -> blockEntity instanceof BellBlockEntity && blockEntity.getPos().isWithinDistance(playerEyes, VILLAGE_RANGE));
-                this.isInVillage = !bell.isEmpty();
+                // We have villagers. Now find a point of interesting (bells or beds)
+                var poi = WorldUtils.getLoadedBlockEntities(world, blockEntity ->
+                        (blockEntity instanceof BellBlockEntity || blockEntity instanceof BedBlockEntity)
+                                && blockEntity.getPos().isWithinDistance(playerEyes, VILLAGE_RANGE));
+
+                this.isInVillage = !poi.isEmpty();
             }
         }
     }
