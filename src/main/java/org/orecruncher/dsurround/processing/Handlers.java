9f0c348ee67dc336e423e7d48c90c53256ceee8f
maksim789546 <maksim_789456@mail.ru>
2022-11-02 07:24:40 +0400

    #12: Add options to disable random sound on startup

--- src/main/java/org/orecruncher/dsurround/processing/Handlers.java
+++ src/main/java/org/orecruncher/dsurround/processing/Handlers.java
@@ -118,6 +118,9 @@ public class Handlers {
             return;
 
         startupSoundPlayed = true;
+        if (!Client.Config.otherOptions.playRandomSoundOnStartup)
+            return;
+
         Client.SoundConfig
                 .getRandomStartupSound()
                 .ifPresent(id -> {
